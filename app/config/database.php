8c86acd1e447ec43fa3f0e57c53ba761b512ad7e
benoit ciret <benoit.ciret@supinternet.fr>
2014-02-10 15:10:05 +0100

    .gitignore conf database

--- app/config/database.php
+++ app/config/database.php
@@ -5,10 +5,10 @@ return array(
     'connections' => array(
         'mysql' => array(
             'driver' => 'mysql',
-            'host' => '',
-            'database' => '',
-            'username' => '',
-            'password' => '',
+            'host' => 'localhost',
+            'database' => 'database1',
+            'username' => 'root',
+            'password' => 'x5stZny/',
             'charset' => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix' => '',
