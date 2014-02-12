95cf8c4daabe196ac33e54fddd2ada542529e026
alicia16489 <kawasaki1>
2014-02-12 16:12:24 +0100

    response config

--- app/config/response.php
+++ app/config/response.php
@@ -5,7 +5,7 @@ return array(
     'prettyPrintJSON' => true,
     'eraseBuffer' => false,
     'REST' => array(
-        'type' => 'html'
+        'type' => 'json'
     ),
     'dieAfterResponse' => false,
     'createXmlFile' => null,
